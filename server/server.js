4aedf05c1667cbe31b20560060d32babab1a6102
Space Invader <amazing.space.invader@gmail.com>
2017-03-15 09:24:57 +0300

    dynamic port

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ import * as invoicesAPI from './api/invoices';
 require('./db');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const isDev = process.env.NODE_ENV !== 'production';
 
 if (isDev) {
